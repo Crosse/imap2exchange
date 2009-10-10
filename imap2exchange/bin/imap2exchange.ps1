0f0893546d31605b6f722cc69ef9dba0f550a985
Seth Wright <seth@crosse.org>
2009-10-10 20:56:05 +0000

    added some logic to determine the amount of memory in the system, and use different java heap sizes, etc., depending on the amount.

--- imap2exchange/bin/imap2exchange.ps1
+++ imap2exchange/bin/imap2exchange.ps1
@@ -20,11 +20,19 @@ $cp="$($cp)$($EXCHANGE_CONVERSION_HOME)\config"
 # Set Java Args
 #################################################
 if ($OPTS) { Remove-Variable OPTS }
-$OPTS= '"-Xms2g" "-Xmx2g" "-Xmn1g" "-Xss128k" '
-$OPTS+='"-XX:ParallelGCThreads=20" "-XX:+UseConcMarkSweepGC" "-XX:+UseParNewGC" '
-$OPTS+='"-XX:SurvivorRatio=8" "-XX:TargetSurvivorRatio=90" "-XX:MaxTenuringThreshold=31" '
-$OPTS+="`"-Dlog4j.configuration=file:///$($EXCHANGE_CONVERSION_HOME)\config\log4j.properties`" "
-$OPTS+="`"-DEXCHANGE_CONVERSION_HOME=$($EXCHANGE_CONVERSION_HOME)`" "
+$OPTS  = ""
+
+if ( ((Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory)/1MB -lt 3072 ) {
+    # For machines with >3GB
+    $OPTS += '"-Xms2g" "-Xmx2g" "-Xmn1g" "-Xss128k" '
+} else { 
+    # For machines with <3G
+    $OPTS += '"-Xms1g" "-Xmx1g" "-Xmn512m" "-Xss128k" '
+}
+$OPTS += '"-XX:ParallelGCThreads=20" "-XX:+UseConcMarkSweepGC" "-XX:+UseParNewGC" '
+$OPTS += '"-XX:SurvivorRatio=8" "-XX:TargetSurvivorRatio=90" "-XX:MaxTenuringThreshold=31" '
+$OPTS += "`"-Dlog4j.configuration=file:///$($EXCHANGE_CONVERSION_HOME)\config\log4j.properties`" "
+$OPTS += "`"-DEXCHANGE_CONVERSION_HOME=$($EXCHANGE_CONVERSION_HOME)`" "
 #$OPTS+='-Dhttp.auth.preference="basic" '
 
 java.exe $OPTS -cp $cp $CLASSNAME $args
