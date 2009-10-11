7f04cc449daae943b6917d901bb2132ee1f3c8fc
Seth Wright <seth@crosse.org>
2009-10-11 16:15:12 +0000

    fixed licenses on some files; probably need to fix licenses on other files, I may have screwed them up.

--- imap2exchange/src/edu/jmu/email/conversion/jmu/JmuSite.java
+++ imap2exchange/src/edu/jmu/email/conversion/jmu/JmuSite.java
@@ -3,40 +3,26 @@ package edu.jmu.email.conversion.jmu;
 import org.apache.log4j.Logger;
 
 /**
- * 
  * <pre>
- * Copyright (c) 2000-2003 James Madison University. All rights reserved.
+ * $URL$
+ * $Author$
+ * $Date$
+ * $Rev$
  * 
- * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE EXPRESSLY
- * DISCLAIMED. IN NO EVENT SHALL JAMES MADISON UNIVERSITY OR ITS EMPLOYEES BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED, THE COSTS OF
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED IN ADVANCE OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- * 
- * Redistribution and use of this software in source or binary forms,
- * with or without modification, are permitted, provided that the
- * following conditions are met:
- * 
- * 1. Any redistribution must include the above copyright notice and
- * disclaimer and this list of conditions in any related documentation
- * and, if feasible, in the redistributed software.
- * 
- * 2. Any redistribution must include the acknowledgment, "This product
- * includes software developed by James Madison University," in any related
- * documentation and, if feasible, in the redistributed software.
- * 
- * 3. The names "JMU" and "James Madison University" must not be used to endorse
- * or promote products derived from this software.
- * </pre>
+ * Copyright (c) 2009 Seth Wright (wrightst@jmu.edu)
  *
-
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * </pre>
  *
  */
 public class JmuSite {
